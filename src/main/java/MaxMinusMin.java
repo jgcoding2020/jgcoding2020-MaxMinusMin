4fc48166b6bcf19cb056e1efc17dcac121d1e22f
Joshua David Gardner <jgardn11@yahoo.com>
2023-02-16 18:56:33 +0000

    Complete

--- src/main/java/MaxMinusMin.java
+++ src/main/java/MaxMinusMin.java
@@ -10,6 +10,16 @@ public class MaxMinusMin {
      * @return the max value of nums minus the min value of nums.
      */
     public int difference(List<Integer> nums){
-        return 0;
+        int max = nums.get(0);
+        int min = max;
+        for (Integer num: nums){
+            if (num < min)
+                min = num;
+            if (num > max)
+                max = num;
+        }      
+        int difference = max - min;
+
+        return difference;
     }
 }
